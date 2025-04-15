2f6425765d58854568786a51376d82d5a556a4ac
HeyTymon <tymon.email.praca@gmail.com>
2025-04-15 23:05:28 +0200

    Automatic commit #15-04-2025_23:05

--- repositoryPush.sh
+++ repositoryPush.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+display_help() {
+ echo "Help for $0:"
+ echo "-p  path to repository"
+ echo "-m  message of the commit"
+ echo "-b  branch"
+ echo "-r  remote"
+}
+
 path=$(pwd)
 message="Automatic commit #$(date +%d-%m-%Y_%H:%M)"
+branch="main"
+remote="origin"
 
-while getopts 'p:m:' opt 2>/dev/null ; do
+while getopts 'p:m:b:r:h' opt 2>/dev/null ; do
  case "$opt" in
   p) path="${OPTARG}" ;;
   m) message="${OPTARG}" ;;
+  b) branch="${OPTARG}" ;;
+  r) remote="${OPTARG}" ;;
+  h) display_help ; exit 0 ;;
   ?) echo "Invalid argument" ; exit 1 ;;
  esac
 done
@@ -23,13 +36,9 @@ IFS=$'\n'
 read -d '' -r -a remotes < <(git remote)
 read -d '' -r -a branches < <(git branch --format '%(refname:short)') #Skip * in the name of the current branch
 
-#echo "${remotes[@]} : ${branches[@]}"
+#TODO - Check if arrays conatins users provided values
 
 git add -A
 git commit -m"${message}"
-git push
-
-#TODO
-#git fetch origin - synchro remote and local branches + user choice where to push chaneges
-
+git push "${remote}" "${branch}"
 
