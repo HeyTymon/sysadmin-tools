40e47a02030e2c8c03db291a316253e61296df7d
HeyTymon <tymon.email.praca@gmail.com>
2025-04-16 10:48:17 +0200

    Automatic commit #16-04-2025_10:48

---
+++ docker_intaller.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ $(/usr/bin/id -u) != "0" ]]; then echo "Run this script as a root" ; exit 1 ; fi
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker run hello-world
+
+docker ps -a
+
+docker rm brave_shannon
